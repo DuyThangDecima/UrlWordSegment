944ab8d808c4fb35b042c018ef3a0a81fdfcb984
duythangdecima <duythangsvbk@gmail.com>
2017-08-15 02:57:22 +0700

    init2

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from wordsegment import segment, clean
 import urllib
 import string
-test = urllib.unquote("https://sites.google.com/site/checkpointinfo2017/?settings%3Ftab=security").decode('utf8')
+import re
+test = urllib.unquote("https://si_tes.google.com/site/checkpointinfo2017/?settings%3Ftab=security").decode('utf8')
 
-print string.punctuation
 def segment_url(url):
     if url.startswith("https"):
         url = url[8:]
     elif url.startswith("http"):
         url = url[7:]
+    list_split = re.split('[_\W]', url)
+    return list_split
 
-
-
-
-print segment('facebokdsdsdsdupdate')
+print segment_url(test)
